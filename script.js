174ac7ab62673049ba199aa5205b354c9143fc12
Einzigartig26 <chaudharirutvik2317@gmail.com>
2022-05-20 12:42:43 +0530

    final

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const newQuoteBtn = document.getElementById("new-quote");
 const twitterBtn = document.getElementById("twitter");
 const authorText = document.getElementById("author");
 const quoteText = document.getElementById("quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuote array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -23,10 +37,13 @@ function newQuote() {
     quoteText.classList.remove("long-quote");
   }
 
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 // Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
